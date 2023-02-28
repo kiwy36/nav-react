02f86f3be4fe5501528e31922c4b7c3453c310ba
Kiwy <kevinwittner92@gmail.com>
2023-02-28 13:29:46 -0300

    version0.1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar/Navbar";
+
+
+function App() {
+  return (
+    <di className="App">
+      <NavBar />
+      <h1>aqui van a ir mas componentes en el futuro cercano</h1>
+      <h2>y mejorar la paleta de colores</h2>
+      <img id="img"src="./kiwi-feliz.jpg"></img>
+    </di>
+  );
+}
+export default App;
+
